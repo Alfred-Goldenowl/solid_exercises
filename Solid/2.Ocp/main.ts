baf8d60084cba1ef6d71c4c413607e04997e6568
Alfred-Goldenowl <alfred.tran.goldenowl@gmail.com>
2024-09-27 10:01:52 +0700

    fix: fix ocp

--- Solid/2.Ocp/main.ts
+++ Solid/2.Ocp/main.ts
@@ -90,25 +90,43 @@ enum EmployeeType {
 }
 
 class Employee {
-    constructor(public name: string, public type: EmployeeType) { }
+    constructor(public name: string) { }
 
+    // calculateSalary(): number {
+    //     switch (this.type) {
+    //         case EmployeeType.FullTime:
+    //             return 5000;
+    //         case EmployeeType.PartTime:
+    //             return 3000;
+    //         case EmployeeType.Intern:
+    //             return 1000;
+    //         default:
+    //             throw new Error("Unknown employee type");
+    //     }
+    // }
+}
+
+class EmployeeFullTime extends Employee{
     calculateSalary(): number {
-        switch (this.type) {
-            case EmployeeType.FullTime:
-                return 5000;
-            case EmployeeType.PartTime:
-                return 3000;
-            case EmployeeType.Intern:
-                return 1000;
-            default:
-                throw new Error("Unknown employee type");
-        }
+        return 5000
     }
 }
 
-const fullTimeEmployee = new Employee("Alice", EmployeeType.FullTime);
+class EmployeePartTime extends Employee{
+    calculateSalary(): number {
+        return 3000
+    }
+}
+
+class EmployeeIntern extends Employee{
+    calculateSalary(): number {
+        return 1000
+    }
+}
+
+const fullTimeEmployee = new EmployeeFullTime("Alice");
 console.log(`${fullTimeEmployee.name}'s salary is ${fullTimeEmployee.calculateSalary()}`);
 
-const internEmployee = new Employee("Bob", EmployeeType.Intern);
+const internEmployee = new EmployeeIntern("Bob");
 console.log(`${internEmployee.name}'s salary is ${internEmployee.calculateSalary()}`);
 /*=========== END PRACTICE 2 ===============*/
